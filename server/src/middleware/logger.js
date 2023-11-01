008da71501f4e0a9ab9f899e6fcb47d8a6bddf0a
stefano-dasilva <stefano.dasilva@mail.polimi.it>
2023-11-01 18:48:56 +0100

    translation

--- server/src/middleware/logger.js
+++ server/src/middleware/logger.js
@@ -1,13 +1,11 @@
 import winston from "winston";
 import WinstonTelegram from "winston-telegram";
 
-// Configura il trasporto di Telegram
 const telegramTransport = new WinstonTelegram({
   token: "6896175365:AAE9OdBc-6jsiQ100BE7Mt7QSTgquByT0cI",
   chatId: "390795682",
 });
 
-// Configura il logger di Winston
 const logger = winston.createLogger({
   transports: [telegramTransport],
 });
