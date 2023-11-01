1bd533348858ed0d1458824c481302fca78bb33d
stefano-dasilva <stefano.dasilva@mail.polimi.it>
2023-11-01 15:19:46 +0100

    changed vercel configuration

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { logroute } from "./src/routes/logger.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "[https://stefanodasilva.vercel.app]",
+    methods: ["GET"],
+  })
+);
+
+app.use("/", logroute);
+
+app.listen(process.env.PORT || 3001, () => console.log("Listening..."));
