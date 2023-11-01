e9f86b0b295bc7852ae806ce924c40ea75816a0b
stefano-dasilva <stefano.dasilva@mail.polimi.it>
2023-11-01 22:02:30 +0100

    changed backend hosting

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
   const handleLogger = async () => {
     try {
       const response = await axios.get(
-        `https://stefano-dasilva-api.onrender.com`
+        `https://stefano-da-silva-api.vercel.app`
       );
       console.log(response);
     } catch (err) {
