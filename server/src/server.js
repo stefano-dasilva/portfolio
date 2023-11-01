74216c1daee0a51b1b38107b66949faeef0e2eba
stefano-dasilva <stefano.dasilva@mail.polimi.it>
2023-11-01 13:07:46 +0100

    changed vercel port

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import { logroute } from "./routes/logger.js";
 import cors from "cors";
 
-const PORT = 3001
 const app = express()
 
 app.use(express.json())
@@ -11,4 +10,4 @@ app.use(cors());
 app.use("/", logroute)
 
 
-app.listen(PORT, () => console.log("Listening..."))
+app.listen(process.env.PORT || 3001, () => console.log("Listening..."))
