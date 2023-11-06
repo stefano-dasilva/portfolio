5040b34953b8fc74d3f8546e3a1413ffaaed1458
stefano-dasilva <stefano.dasilva@mail.polimi.it>
2023-11-06 19:44:39 +0100

    changed mongodburi

--- server/index.js
+++ server/index.js
@@ -10,13 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://stefanodasilva.vercel.app", "http://localhost:3000"],
+    origin: ["https://stefanodasilva.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 
 app.use("/", logroute);
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URI);
 
 app.listen(process.env.PORT || 3001, () => console.log("Listening..."));
