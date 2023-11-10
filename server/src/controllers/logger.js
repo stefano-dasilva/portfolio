7cd3743201a40e6290fdd9407f725cbe4a7e258c
stefano-dasilva <stefano.dasilva@mail.polimi.it>
2023-11-10 11:17:31 +0100

    changed logger sett

--- server/src/controllers/logger.js
+++ server/src/controllers/logger.js
@@ -2,8 +2,7 @@ import winston from "winston";
 import WinstonTelegram from "winston-telegram";
 import { LoggerModel } from "../models/LoggerModel.js";
 const log = async (req, res) => {
-
-    const {date} = req.body
+  const { date } = req.body;
 
   try {
     const telegramTransport = new WinstonTelegram({
@@ -13,24 +12,25 @@ const log = async (req, res) => {
 
     telegramTransport.setMaxListeners(20);
 
+    const clientIP =
+      req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
     const logger = winston.createLogger({
       transports: [telegramTransport],
     });
-    const logmessage = `${new Date()} - IP: ${req.ip} - ${req.method} ${
+    const logmessage = `${new Date()} - IP: ${clientIP} - ${req.method} ${
       req.url
     }\n`;
     logger.info(logmessage);
-
 
     const visit_IP = req.ip;
 
     const newlog = new LoggerModel({
       date: date,
-      IP: visit_IP,
+      IP: clientIP,
     });
 
-    await newlog.save()
-
+    await newlog.save();
 
     return res.status(201).json({ message: "OK", date: new Date() });
   } catch (err) {
